ddd0a0c225d271330b7cc210a1f32db1b80c9047
technicollins <upendocollins12@gmail.com>
2020-10-27 11:13:51 +0300

    made the UI better

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -7,19 +7,26 @@ class addTransaction(forms.ModelForm):
 
 	def __init__(self, chamaID, *args, **kwargs):
 		super(addTransaction, self).__init__(*args, **kwargs)
-		self.fields['memberID'] = forms.ModelChoiceField(queryset = models.ChamaMembers.objects.filter(chamaID = chamaID, user__is_active = True), empty_label = "Choose a member")
+		self.fields['memberID'] = forms.ModelChoiceField(queryset = models.ChamaMembers.objects.filter(chamaID = chamaID, user__is_active = True), empty_label = "Choose a member", widget = forms.Select(attrs = {'class':'form-control'}))
 
 	class Meta:
 		model = models.Transactions
 		fields = ['memberID', 'transactionType', 'amount']
 		labels = {'memberID': 'Member ID', 'transactionType': 'Transaction Type', 'amount': 'Amount'}
+		widgets = {
+			'amount':forms.NumberInput(attrs = {'class':'form-control'}),
+			'transactionType':forms.Select(attrs = {'class':'form-control'}),
+		}
 
 class loanForm(forms.ModelForm):
 
 	def __init__(self, chamaID, *args, **kwargs):
 		super(loanForm, self).__init__(*args, **kwargs)
-		self.fields['memberID'] = forms.ModelChoiceField(queryset = models.ChamaMembers.objects.filter(chamaID = chamaID, user__is_active = True), empty_label = "Choose a member")
+		self.fields['memberID'] = forms.ModelChoiceField(queryset = models.ChamaMembers.objects.filter(chamaID = chamaID, user__is_active = True), empty_label = "Choose a member", widget = forms.Select(attrs = {'class':'form-control'}))
 
 	class Meta:
 		model = models.Loans
 		fields = ['memberID', 'amount']
+		widgets = {
+			'amount':forms.NumberInput(attrs = {'class': 'form-control'}),
+		}
